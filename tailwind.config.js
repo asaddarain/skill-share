471263035fba3b7817b31d97bcbcaf5982b5132a
Asad Darain <asaddarain8@gmail.com>
2022-04-30 04:55:25 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      color: {
+        primary: '#9374f0',
+        secondary: '#665fef'
+      }
+    },
+  plugins: [],
+}
+}
